425527ce352e6383e5a0df2ac027775ef265dd4c
Ilkin Safarli <ilkinsafarli@alone.local>
2016-04-01 19:59:42 +0300

    Updated web.js

--- web.js
+++ web.js
@@ -1,13 +1,15 @@
-var express = require('express')
-var app = express()
-
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+r express = require('express');
+var app = express();
+var fs = require('fs');
+app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World! 2')
-})
+	  buf = fs.readFileSync('index.html');
+	    myString = buf.toString();
+	      response.send(myString);
+});
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+	  console.log("Listening on " + port);
+});
